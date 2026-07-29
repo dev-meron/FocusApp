8e63a3956ca7b2ef613269c88c3eef9d6b1774c9
Meron <merijourney97@gmail.com>
2026-07-30 02:16:47 +0300

    add styles for the screens

--- App.js
+++ App.js
@@ -1,53 +1,113 @@
-import {View, Text, StyleSheet , TouchableOpacity} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import {TextInput} from 'react-native-paper';
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { TextInput } from "react-native-paper";
+import FocusTime from "./components/focusTime";
 
-export default App(){
-  return(
-    <SafeAreaView style ={styles.container}>
-     <View style={styles.inputcontainer}>
-       <TextInput
-       placeholder = "what would you like focus....."
-       mode = {"outlined"}
-       label = "Focus"
-       style = {styles.inputtext}
-       />
-       <TouchableOpacity style={styles.fabButton}>
-       <Text style ={styles.fabText}>+</Text>
-       </TouchableOpacity>
-     </View>
+export default function App() {
+  const [switchScreen, setSwitchScreen] = useState(false);
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [selectedTask, setSelectedTask] = useState("");
+
+  const changeScreen = () => {
+    setSwitchScreen(!switchScreen);
+  };
+
+  const addTask = () => {
+    const trimmed = task.trim();
+
+    if (trimmed.length > 0) {
+      setTasks((prev) => [...prev, trimmed]);
+      setTasks("");
+      setSelectedTask(trimmed);
+    }
+  };
+
+  if (switchScreen) {
+    return <FocusTime focusTask={selectedTask} onBack={changeScreen} />;
+  }
+
+  const handleBack = () => {
+    setAddTask((prev) => !prev);
+  };
+
+  const handleTextChange = () => {
+    setTask(task);
+    setTask("");
+  };
+
+  if (addTask) {
+    return <FocusTime />;
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          placeholder={"what would you like focus....."}
+          mode={"outlined"}
+          label="Focus"
+          style={styles.inputtext}
+          value={task}
+          onChange={(text) => setTask(text)}
+        />
+        <TouchableOpacity
+          style={styles.fabButton}
+          onPress={() => {
+            addTask();
+            changeScreen();
+          }}
+        >
+          <Text style={styles.fabText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.focusedTasks}>
+        <Text style={styles.focudTitle}>Things we have focused on:</Text>
+        {task.map((task, index) => (
+          <text key={index} style={styles.taskText}>
+            {task}
+          </text>
+        ))}
+      </View>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex : 1,
-    backgroundColor :'#090437',
+  container: {
+    flex: 1,
+    backgroundColor: "#090437",
   },
-  Text: {
-    marginTOP: 50,
+
+  inputcontainer: {
+    flexdirection: "row",
+    padding: 20,
+    backgroundColor: "#fff",
   },
-  inputcontainer :{
-    flexdirection:'row',
-    padding : 20
+  inputtext: {
+    flex: 1,
   },
-  inputtext:{
-    flex:1
-  },
-  fabButton :{
-    height:60,
-    width:60,
+  fabButton: {
+    height: 60,
+    width: 60,
     borderRadius: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor:'transparent',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
     borderWidth: 2,
-    borderColor: '#fff',
+    borderColor: "#fff",
     marginLeft: 10,
   },
   fabText: {
     fontSize: 20,
-    color: '#fff'
-  }
-})
+    color: "#fff",
+  },
+  taskText: {
+    fontWeight: "semibold",
+    fontSize: 18,
+    color: "white",
+    padding: 10,
+  },
+});
---
+++ components/focusTime.js
@@ -0,0 +1,115 @@
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useState, useEffect } from "react";
+
+export default function FocusTime({ focusTask, onBack }) {
+  const times = [600, 900, 1200];
+  const [isRunning, setIsRunning] = useState(false);
+  const [selectedTime, setSelectedTime] = useState(null);
+
+  const timeFormat = (times) => {
+    const minute = Math.floor(times / 60);
+    const second = Math.floor(times % 60);
+
+    return `${minute}:${second < 10 ? "0" : ""}${second}`;
+  };
+
+  useEffect(() => {});
+
+  return (
+    <SafeAreaView styles={styles.container}>
+      <Text style={styles.timerText}>{timeFormat(times[0])}</Text>
+      <Text style={styles.subTitle}>Focusing on:</Text>
+      <Text style={styles.focusTask}>{focusTask}</Text>
+
+      <View style={{height:10, width:100%, backgroundColor:'#241b9a, marginTop:30, marginBottom:20,'}}/>
+
+
+      <View style={styles.timeoption}>
+        {times.map((time, index)=>(
+          <TouchableOpacity key={index} style={styles.timeoptionsButton} onPress={() =>{}}>
+            <Text style={styles.timeoptionText}>{timeFormat(time)}</Text>
+          </TouchableOpacity>
+         ))}
+      </View>
+
+      <TouchableOpacity style={styles.startFab}>
+        <Text style={{color:'white'}}>{isRunning? 'Paused' :'Start'}</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.backBotton} onPress={onBack}>
+        <Text style={{color:'white'}}>Back</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#252250",
+    alignItems: "center",
+  },
+  timerText: {
+    fontWeight: "bold",
+    fontSize: 60,
+    color: "white",
+    marginTop: 50,
+  },
+  subTitle: {
+    fontSize: 18,
+    color: "white",
+    marginTop: 50,
+  },
+  focusTask: {
+    fontSize: 30,
+    color: 'white',
+    marginTop:20,
+    fontWeight: 'bold',
+
+  },
+  timeoption :{
+    flexDirection: 'row',
+    marginTop:30,
+    alignItems:'space-between',
+    width : '100%',
+    justifyContent :'space-around',
+  },
+  timeoptionsButton :{
+    height: 100,
+    width: 100,
+    borderRadius:50,
+    justifyContent:'center',
+    alignContent: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderColor: 'white',
+
+  },
+  timeoptionText: {
+    color: 'white',
+    fontSize: 18,
+    
+  },
+  startFab:{
+    height:80,
+    width:80, 
+    borderRadius:40,
+    backgroundColor:'transparent',
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  backBotton :{
+    marginTop: 50,
+    height:50,
+    width:100,
+    borderRadius:25,
+    backgroundColor: 'transparent',
+    borderWidth:2,
+    borderColor: 'white',
+    justifyContent:'center',
+    alignItems: 'center'
+  }
+});
